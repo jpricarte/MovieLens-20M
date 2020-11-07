36f04321df4dc6e2a2eedd33d5ec114529065045
Jordi Pujol Ricarte <jpricarte@inf.ufrgs.br>
2020-11-07 10:57:42 -0300

    Refact Trie

---
+++ src/structs/Trie/Trie.java
@@ -0,0 +1,73 @@
+package structs.Trie;
+
+import java.util.LinkedList;
+
+public class Trie{
+    private MovieNode root;
+
+    public Trie() {
+        this.root = new MovieNode();
+    }
+
+    public void insert(String movieName, int movieId) {
+        MovieNode aux = root;
+        movieName = movieName.trim().toLowerCase();
+        // Faz apenas o caminhamento
+        for (int i=0; i < movieName.length(); i++) {
+            char c = (char) (movieName.charAt(i) & 0xFF);
+
+
+            MovieNode nextNode = aux.getChildren()[c];
+            if(nextNode == null) {
+                aux.getChildren()[c] = new MovieNode(c);
+            }
+
+            aux = aux.getChildren()[c];
+        }
+
+        // Foi até o ultimo nodo, agora insere filme
+        aux.setMovieId(movieId);
+        aux.setLeaf(true);
+    }
+
+    public int getMovie(String movieName) {
+        MovieNode aux = root;
+        movieName = movieName.trim().toLowerCase();
+        // Faz apenas o caminhamento
+        for (int i=0; i < movieName.length(); i++) {
+            char c = (char) (movieName.charAt(i) & 0xFF);
+
+            MovieNode nextNode = aux.getChildren()[c];
+
+            if(nextNode == null) {
+                return -1;
+            }
+
+            aux = aux.getChildren()[c];
+        }
+
+        return aux.getMovieId();
+    }
+
+    public LinkedList<Integer> getAllMovies(String prefix) {
+        MovieNode aux = root;
+        prefix = prefix.trim().toLowerCase();
+
+        // Faz apenas o caminhamento
+        for (int i=0; i < prefix.length(); i++) {
+
+            char c = (char) (prefix.charAt(i) & 0xFF);
+
+            MovieNode nextNode = aux.getChildren()[c];
+
+            if(nextNode == null) {
+                return new LinkedList<Integer>();
+            }
+
+            aux = aux.getChildren()[c];
+        }
+
+        return aux.getSubTree();
+    }
+
+}
--- src/structs/Trie/TrieObject.java
+++ src/structs/Trie/TrieObject.java
@@ -5,5 +5,8 @@ import csvReader.structs.Movie;
 import java.util.List;
 
 public interface TrieObject {
-    public ArvoreTrie popularTrie(List<Movie> movieList);
+//    public ArvoreTrie popularTrie(List<Movie> movieList);
+    Object[] getChildren();
+    boolean isLeaf();
+    void setLeaf(boolean leaf);
 }
